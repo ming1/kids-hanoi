a84ce7dff089d0d6f55250f5b38dcfa624984dfa
Ming Lei <tom.leiming@gmail.com>
2016-08-28 07:55:07 +0800

    js: hanoi: continue to cleanup
    Signed-off-by: Ming Lei <tom.leiming@gmail.com>

--- js/towersOfHanoi.js
+++ js/towersOfHanoi.js
@@ -63,7 +63,7 @@
 
 	  function setup_game() {
 	     nr_discs = parseInt(document.form1.nr_discs.value);
-		 delay = document.form1.move_delay.value;
+		 delay = parseInt(document.form1.move_delay.value);
 
 		 tower_h = 140 + nr_discs * disc_h;
 		 tower_w = 189 + nr_discs * 7
@@ -82,29 +82,16 @@
 		 add_discs();
 	  }
 
+      function init_drag() {
+      }
+
+      function init_drop() {
+      }
+
       function initialize() {
-        initDrag();
-        initDrop();
+        init_drag();
+        init_drop();
 		setup_game();
-      }
-
-      function initDrag() {
-      }
-
-      function initDrop() {
-      }
-
-      function isDraggingAllowed(parent, child) {
-        return $(parent).children()[0].id == child.id;
-      }
-
-      function isValidMove(parent, child) {
-        var children = $(parent).children();
-        return (children.length == 0) || (children.css(ATTR_WIDTH) >= child.css(ATTR_WIDTH));
-      }
-
-      function isDone(parent) {
-        return ((parent.id == ID_LAST_TOWER) && ($(parent).find(SELECT_CONTENT).children().length == NUMBER_OF_DISCS));
       }
 
 	  function auto_play() {
